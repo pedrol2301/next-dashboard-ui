cdba6d1a551c2f41e49b80dfdb37c7c4b4699675
pedrolima <pedrolima@biofy.tech>
2025-01-27 17:02:14 -0300

    start project

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,19 @@
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+      <div className="h-screen flex">
+        {/* LEFT */}
+        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-blue-500">
+          l
+        </div>
+        {/* RIGHT */}
+        <div className="w-[86%] md:w-[92%] lg:w-[86%] xl:w-[86%] bg-red-500">
+          r
+        </div>
+      </div>
+  );
+}
